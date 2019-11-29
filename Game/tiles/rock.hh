6c13c1166086f2f06c430d488c94cfc8d76b0632
Benjamin-Kavanagh <benjamin.kavanagh@tuni.fi>
2019-11-29 21:36:37 +0200

    Change number of placeable buildings on rock

--- Game/tiles/rock.hh
+++ Game/tiles/rock.hh
@@ -16,7 +16,7 @@
  * * Ore = 2
  *
  *
- * Tile supports 4 buildings.
+ * Tile supports 1 buildings.
  */
 class Rock : public Course::TileBase
 {
@@ -36,7 +36,7 @@ public:
     Rock(const Course::Coordinate& location,
          const std::shared_ptr<Course::iGameEventHandler>& eventhandler,
          const std::shared_ptr<Course::iObjectManager>& objectmanager,
-         const unsigned int& max_build = 2,
+         const unsigned int& max_build = 1,
          const unsigned int& max_work = 3,
          const Course::ResourceMap& production = NewResourceMaps::ROCK_BP);
 
